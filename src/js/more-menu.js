06baed524974c1d823b5320fe34dbdf20e225acc
Matthew Brabham <matthew.brabham@oregonstate.edu>
2021-02-01 11:03:22 -0800

    Update Template variables to use snake case. Add More styling for Group Menu

--- src/js/more-menu.js
+++ src/js/more-menu.js
@@ -1,76 +1,91 @@
-const menuContainer = document.querySelector('#block-madrone-main-menu');
-const menuPrimary = document.querySelector('.madrone-mega-menu-main');
-const menuPrimaryItems = menuContainer.querySelectorAll(
-  '.madrone-mega-menu-main > li:not(.madrone-mega-menu-main-more)'
-);
-
-const createMoreMenu = () => {
-  menuContainer.classList.add('--jsfied');
-  menuPrimary.insertAdjacentHTML(
-    'beforeend',
-    `
-  <li class="madrone-mega-menu-main-more tw-hidden">
+/**
+ *
+ * @param blockId
+ * @param navClass
+ */
+const createMoreMenu = (blockId, navClass) => {
+  const menuContainer = document.querySelector('#' + blockId);
+  const menuPrimary = document.querySelector('.' + navClass);
+  if (menuContainer && menuPrimary) {
+    menuContainer.classList.add('--jsfied');
+    menuPrimary.insertAdjacentHTML(
+      'beforeend',
+      `
+  <li class="${navClass}-more tw-hidden">
       <button type="button" aria-haspopup="true" aria-expanded="false">
           More <i class="fas fa-fw fa-ellipsis-h"></i>
       </button>
-      <ul class="madrone-mega-menu-main-secondary">
+      <ul class="${navClass}-secondary">
           ${menuPrimary.innerHTML}
       </ul>
       </button>
   </li>
   `
-  );
-
-  const moreLi = menuPrimary.querySelector('.madrone-mega-menu-main-more');
-  const moreButton = moreLi.querySelector('button');
-
-  moreButton.addEventListener('click', event => {
-    event.preventDefault();
-    menuContainer.classList.toggle('madrone-mega-menu-main-show-secondary');
-    moreButton.setAttribute(
-      'aria-expanded',
-      menuContainer.classList.contains('madrone-mega-menu-main-show-secondary')
     );
-  });
-};
 
-const adjustMoreMenu = () => {
-  const allItems = menuContainer.querySelectorAll('li');
-  const moreLi = menuPrimary.querySelector('.madrone-mega-menu-main-more');
-  const moreButton = moreLi.querySelector('button');
+    const moreLi = menuPrimary.querySelector('.' + navClass + '-more');
+    const moreButton = moreLi.querySelector('button');
 
-  allItems.forEach(item => {
-    item.classList.remove('tw-hidden');
-  });
-  let stopWidth = moreButton.offsetWidth + 45;
-
-  let primaryHiddenItems = [];
-  const primaryWidth = menuPrimary.offsetWidth - stopWidth;
-  menuPrimary.style.width = primaryWidth;
-  menuPrimaryItems.forEach((item, i) => {
-    if (primaryWidth >= stopWidth + item.offsetWidth) {
-      stopWidth += item.offsetWidth + 45;
-    } else {
-      item.classList.add('tw-hidden');
-      primaryHiddenItems.push(item.getAttribute('data-index'));
-    }
-  });
-  if (!primaryHiddenItems.length) {
-    moreLi.classList.add('tw-hidden');
-    menuContainer.classList.remove('madrone-mega-menu-main-show-secondary');
-    moreButton.setAttribute('aria-expanded', false);
-  } else {
-    const menuSecondary = menuContainer.querySelector(
-      '.madrone-mega-menu-main-secondary'
-    );
-    const menuSecondaryItems = menuSecondary.querySelectorAll(':scope > li');
-    menuSecondaryItems.forEach((item, i) => {
-      item.classList.remove('tw-whitespace-nowrap'); // "more menu" items should wrap to multiple lines if needed
-      if (!primaryHiddenItems.includes(item.getAttribute('data-index'))) {
-        item.classList.add('tw-hidden');
-      }
+    moreButton.addEventListener('click', event => {
+      event.preventDefault();
+      menuContainer.classList.toggle(navClass + '-show-secondary');
+      moreButton.setAttribute(
+        'aria-expanded',
+        menuContainer.classList.contains(navClass + '-show-secondary')
+      );
     });
   }
 };
+/**
+ *
+ * @param blockId
+ * @param navClass
+ */
+const adjustMoreMenu = (blockId, navClass) => {
+  const menuContainer = document.querySelector('#' + blockId);
+  const menuPrimary = document.querySelector('.' + navClass);
+  if (menuContainer && menuPrimary) {
+    const menuPrimaryItems = menuContainer.querySelectorAll(
+      `.${navClass} > li:not(.${navClass}-more)`
+    );
 
-export { createMoreMenu, adjustMoreMenu };
+    const allItems = menuContainer.querySelectorAll('li');
+    const moreLi = menuPrimary.querySelector('.' + navClass + '-more');
+    const moreButton = moreLi.querySelector('button');
+
+    allItems.forEach(item => {
+      item.classList.remove('tw-hidden');
+    });
+    let stopWidth = moreButton.offsetWidth + 45;
+
+    let primaryHiddenItems = [];
+    const primaryWidth = menuPrimary.offsetWidth - stopWidth;
+    menuPrimary.style.width = primaryWidth;
+    menuPrimaryItems.forEach((item, i) => {
+      if (primaryWidth >= stopWidth + item.offsetWidth) {
+        stopWidth += item.offsetWidth + 45;
+      } else {
+        item.classList.add('tw-hidden');
+        primaryHiddenItems.push(item.getAttribute('data-index'));
+      }
+    });
+    if (!primaryHiddenItems.length) {
+      moreLi.classList.add('tw-hidden');
+      menuContainer.classList.remove(navClass + '-show-secondary');
+      moreButton.setAttribute('aria-expanded', false);
+    } else {
+      const menuSecondary = menuContainer.querySelector(
+        '.' + navClass + '-secondary'
+      );
+      const menuSecondaryItems = menuSecondary.querySelectorAll(':scope > li');
+      menuSecondaryItems.forEach((item, i) => {
+        item.classList.remove('tw-whitespace-nowrap'); // "more menu" items should wrap to multiple lines if needed
+        if (!primaryHiddenItems.includes(item.getAttribute('data-index'))) {
+          item.classList.add('tw-hidden');
+        }
+      });
+    }
+  }
+};
+
+export {createMoreMenu, adjustMoreMenu};
