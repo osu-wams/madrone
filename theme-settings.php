bfcc6c9f54bbf62e277634096372925b1d11c81b
Nick Piatt <developer.piatt@gmail.com>
2021-03-01 10:42:02 -0800

    fixed coding standards in theme-settings.php

--- theme-settings.php
+++ theme-settings.php
@@ -5,16 +5,18 @@
  * Theme settings can be found here.
  */
 
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function madrone_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id = NULL) {
+function madrone_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
   }
 
-  $form ['madrone_front_title'] = [
+  $form['madrone_front_title'] = [
     '#type' => 'checkbox',
     '#title' => t('Hide Title'),
     '#default_value' => theme_get_setting('madrone_front_title'),
