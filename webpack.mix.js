7991af6c3dbab3f943b35bb5724e5e6b946316d7
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2021-03-08 14:36:30 -0800

    Cleans up webpack and tailwind configs, and enables purge by default. Commits the new purge css file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,11 @@
-let glob = require('glob-all');
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
-require('laravel-mix-purgecss');
-
-mix.options({ processCssUrls: false });
-
-mix.postCss('src/madrone.css', 'dist', [require('tailwindcss')]);
-mix.js('src/madrone.js', 'dist');
-
-mix.purgeCss({
-  enabled: false, // Remove this line to enable PurgeCSS.
-  paths: () =>
-    glob.sync([
-      path.join(__dirname, '**/*.twig'),
-      '/var/www/sites/*/{modules,themes}/custom/**/*.twig',
-      '/var/www/{modules,themes}/custom/**/*.twig'
-    ]),
-  whitelistPatterns: [],
-  whitelistPatternsChildren: []
-});
+mix
+  .js('src/madrone.js', 'dist')
+  .postCss('src/madrone.css', 'dist', [
+    require('tailwindcss'),
+    require('autoprefixer')
+  ])
+  .options({
+    processCssUrls: false
+  });
