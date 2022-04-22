3ddb2cbf0e30a6a1948bf9113f60097c3de4f92f
Alex Ruef <alex.ruef@oregonstate.edu>
2022-04-22 10:19:47 -0700

    Added js to control group mobile menu

--- src/madrone.js
+++ src/madrone.js
@@ -13,7 +13,6 @@ window.addEventListener('load', () => {
 
   // handle group menu horizontal spacing
   const groupMenus = [...document.querySelectorAll('.block-group-content-menu ul .menu--__submenu')];
-  // groupMenus.shift();
   groupMenus.forEach(menu => {
     if (!menu.classList.contains('menu--level-2')) {
       menu.addEventListener("mouseenter", function(event) {
@@ -21,6 +20,8 @@ window.addEventListener('load', () => {
       });
     }
   });
+
+  groupMobileMenu();
 });
 
 /**
@@ -36,4 +37,75 @@ function menuClickedText(sfToggle) {
   }
 }
 
+function groupMobileMenu() {
+  if (isMobile()) {
+    const menu = document.querySelectorAll('.menu--level-1')[0];
 
+    // Create menu top level bucket
+    createMenuBucket(menu);
+
+    const liList = [...document.querySelectorAll('#block-groupmenu .menu--level-1 li')];
+    liList.forEach(li => {
+      // if li has child ul element
+      if ([...(li.children)].some(e => e.tagName === 'UL')) {
+        cloneLi(li);
+
+        // add class chevron icon
+        li.classList.add('group-sub-menu');
+
+        li.children[0].addEventListener('click', menuItemClickEvent);
+      }
+    });
+  }
+}
+
+function createMenuBucket(menu) {
+  const topUl = document.createElement('ul');
+  topUl.classList = menu.classList;
+  topUl.classList.remove('menu--level-1');
+  topUl.classList.add('menu--level-0', 'group-mobile-menu');
+
+  const topLi = document.createElement('li');
+  topLi.classList = menu.children[1].classList;
+
+  const topA = document.createElement('a');
+  topA.classList = menu.children[1].children[0].classList;
+  topA.classList.add('group-mobile-main-a');
+  topA.innerHTML = 'Group Menu';
+
+  topLi.appendChild(topA);
+  topUl.appendChild(topLi);
+  menu.parentNode.insertBefore(topUl, menu);
+  topLi.appendChild(menu);
+
+  topA.addEventListener('click', menuItemClickEvent);
+}
+
+function menuItemClickEvent(event) {
+  // prevent clicks from navigating to the a href
+  event.preventDefault();
+
+  // add styling and change text when clicked
+  if (this.parentNode.classList.contains('group-menu-clicked')) {
+    this.parentNode.classList.remove('group-menu-clicked');
+
+    this.textContent = this.textContent.replace('Close ', '');
+  } else {
+    this.parentNode.classList.add('group-menu-clicked');
+
+    const menuToggleText = this.textContent;
+    this.textContent = `Close ${menuToggleText}`;
+  }
+}
+
+// clone li and add to child ul element
+function cloneLi(li) {
+  const clonedA = li.children[0].cloneNode(true);
+  const clonedLi = document.createElement('li');
+  clonedLi.appendChild(clonedA);
+  li.children[1].prepend(clonedLi);
+}
+
+function isMobile() {
+  return navigator.userAgent.match(/(android|bb\d+|meego)|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i);
+}
