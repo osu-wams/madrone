319238bc3d52f94dd89a6ecee383287ba45ab539
Matthew Brabham <matthew.brabham@oregonstate.edu>
2022-10-05 13:59:46 -0700

    Add option to hide breadcrumbs. Remove option to hide front page title.

--- theme-settings.php
+++ theme-settings.php
@@ -20,16 +20,16 @@ function madrone_form_system_theme_settings_alter(&$form, FormStateInterface $fo
     '#type' => 'fieldset',
     '#title' => t('Madrone Utilities'),
   ];
-  $form['madrone_settings']['madrone_utilities']['madrone_front_title'] = [
+  $form['madrone_settings']['madrone_utilities']['madrone_hide_breadcrumbs'] = [
     '#type' => 'checkbox',
-    '#title' => t('Hide Title'),
-    '#default_value' => theme_get_setting('madrone_front_title'),
-    '#description' => t('Hide the node title on front page.'),
+    '#title' => t('Hide Breadcrumbs'),
+    '#default_value' => theme_get_setting('madrone_hide_breadcrumbs'),
+    '#description' => t('Enable to hide breadcrumbs in your site'),
   ];
   $form['madrone_settings']['madrone_utilities']['madrone_monsido_site_id'] = [
-    '#type'          => 'textfield',
-    '#title'         => t('Monsido Site Id'),
+    '#type' => 'textfield',
+    '#title' => t('Monsido Site Id'),
     '#default_value' => theme_get_setting('madrone_monsido_site_id'),
-    '#description'   => t("Site Id for monsido analytics"),
+    '#description' => t("Site Id for monsido analytics"),
   ];
 }
