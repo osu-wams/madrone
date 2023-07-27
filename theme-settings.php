e65d8441bf7fe1c67f85c51ee47f036704c83724
Matthew Brabham <matthew.brabham@oregonstate.edu>
2023-07-27 08:51:22 -0700

    Remove the Logo and Favicon field options.

--- theme-settings.php
+++ theme-settings.php
@@ -15,15 +15,16 @@ function madrone_form_system_theme_settings_alter(&$form, FormStateInterface $fo
   if (isset($form_id)) {
     return;
   }
-
+  $form['logo']['#access'] = FALSE;
+  $form['favicon']['#access'] = FALSE;
   $form['madrone_settings']['madrone_utilities'] = [
     '#type' => 'fieldset',
     '#title' => t('Madrone Utilities'),
   ];
   $form['madrone_settings']['madrone_utilities']['madrone_monsido_site_id'] = [
-    '#type'          => 'textfield',
-    '#title'         => t('Monsido Site Id'),
+    '#type' => 'textfield',
+    '#title' => t('Monsido Site Id'),
     '#default_value' => theme_get_setting('madrone_monsido_site_id'),
-    '#description'   => t("Site Id for monsido analytics"),
+    '#description' => t("Site Id for monsido analytics"),
   ];
 }
